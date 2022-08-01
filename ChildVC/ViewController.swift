8fd26a1cecdbbb1fa491e7ed8d7b1a3f003bc981
komplektan <komplektan@mail.ru>
2022-08-01 16:56:03 +0500

    Initial Commit

---
+++ ChildVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChildVC
+//
+//  Created by Вячеслав Квашнин on 01.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
